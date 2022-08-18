8f3eba76e36a217cf7abc6d10360cf1158102757
Tuncvrjs <90102420+Tuncvrjs@users.noreply.github.com>
2022-08-18 10:14:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express")
 const app = express()
 app.get("/", (req, res) => {
 
-res.sendFile("./index.html")
+res.sendFile("./index.html", {root: __dirname} )
 })
 app.listen(3000)
